3a70f218872edec2fb5d9de12fd407003c8b6087
Greg.Study <greg0917@naver.com>
2022-04-22 02:01:07 +0900

    *CORE* class 네이밍 리팩토링

--- core/src/test/java/hello/core/order/OrderServiceImplTest.java
+++ core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -1,10 +1,9 @@
 package hello.core.order;
 
-import hello.core.discount.FIxDiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -16,7 +15,7 @@ public class OrderServiceImplTest {
         MemoryMemberRepository memberRepository = new MemoryMemberRepository();
         memberRepository.save(new Member(1L, "greg", Grade.VIP));
 
-        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FIxDiscountPolicy());
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
         Order order = orderService.createOrder(1L, "itemA", 10000);
         assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
