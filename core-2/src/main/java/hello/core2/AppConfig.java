5c7c2f2e5b5f27554a2d7627f15c17f2d19617e9
Greg.Study <greg0917@naver.com>
2022-08-27 02:06:55 +0900

    bean 등록

--- core-2/src/main/java/hello/core2/AppConfig.java
+++ core-2/src/main/java/hello/core2/AppConfig.java
@@ -1,28 +1,35 @@
 package hello.core2;
 
 import hello.core2.discount.DiscountPolicy;
-import hello.core2.discount.FixDiscountPolicy;
+import hello.core2.discount.RateDiscountPolicy;
 import hello.core2.member.MemberRepository;
 import hello.core2.member.MemberService;
 import hello.core2.member.MemberServiceImpl;
 import hello.core2.member.MemoryMemberRepository;
 import hello.core2.order.OrderService;
 import hello.core2.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
