c35dd2d21929b996ac367b2352d8ed66eef499d6
Greg.Study <greg0917@naver.com>
2022-09-02 02:51:23 +0900

    생성자주입 테스트

--- core-2/src/main/java/hello/core2/order/OrderServiceImpl.java
+++ core-2/src/main/java/hello/core2/order/OrderServiceImpl.java
@@ -3,7 +3,6 @@ package hello.core2.order;
 import hello.core2.discount.DiscountPolicy;
 import hello.core2.member.Member;
 import hello.core2.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +12,6 @@ public class OrderServiceImpl implements OrderService {
 
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
