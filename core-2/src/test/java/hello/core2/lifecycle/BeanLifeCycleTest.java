3dd3fe7a53f6bb2f7399b050d017613c5414731c
Greg.Study <greg0917@naver.com>
2022-09-03 01:20:59 +0900

    Bean LifeCycle - @PostConstruct, @PreDestroy

--- core-2/src/test/java/hello/core2/lifecycle/BeanLifeCycleTest.java
+++ core-2/src/test/java/hello/core2/lifecycle/BeanLifeCycleTest.java
@@ -11,7 +11,7 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LiftCycleConfig {
 
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://www.hello.co.kr");
