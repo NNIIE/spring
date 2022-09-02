1e594b870ea64b8840fda56ef4c098e3550f7ea4
Greg.Study <greg0917@naver.com>
2022-09-03 00:36:58 +0900

    Bean LifeCycle - implements InitializingBean, DisposableBean

---
+++ core-2/src/test/java/hello/core2/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package hello.core2.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Configuration
+    static class LiftCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://www.hello.co.kr");
+            return networkClient;
+        }
+    }
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LiftCycleConfig.class);
+        NetworkClient network = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+}
