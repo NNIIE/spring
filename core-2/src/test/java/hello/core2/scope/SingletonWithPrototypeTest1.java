b3a1621266532a189f6485bb861b32bc77ff2b2f
Greg.Study <greg0917@naver.com>
2022-09-03 17:10:21 +0900

    Provider

--- core-2/src/test/java/hello/core2/scope/SingletonWithPrototypeTest1.java
+++ core-2/src/test/java/hello/core2/scope/SingletonWithPrototypeTest1.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,10 +41,10 @@ public class SingletonWithPrototypeTest1 {
     @RequiredArgsConstructor
     static class ClientBean {
 
-        private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private final Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
